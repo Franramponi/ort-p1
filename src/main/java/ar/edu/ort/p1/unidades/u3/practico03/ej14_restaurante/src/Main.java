29744ddfb94cf4e1d369553ce52eab539cdcfcae
Francisco Ramponi <ramponifran@gmail.com>
2023-04-21 16:28:04 -0300

    Update Main.java
    Esta vacio el main

--- src/main/java/ar/edu/ort/p1/unidades/u3/practico03/ej14_restaurante/src/Main.java
+++ src/main/java/ar/edu/ort/p1/unidades/u3/practico03/ej14_restaurante/src/Main.java
@@ -3,7 +3,7 @@ package ar.edu.ort.p1.unidades.u3.practico03.ej14_restaurante.src;
 public class Main {
 
 	public static void main(String[] args) {
-		//esta vacio!!!!
+		// TODO Auto-generated method stub
 
 	}
 
